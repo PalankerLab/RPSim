f2896a158c0d7625d6dee0cc01277aa1491c4628
Charles Chen <cccharleschan@gmail.com>
2023-03-01 22:53:22 -0800

    Conductance matrix simplification
    Monopolar mode only: downsize the resistor mesh by configuration["r_matrix_simp_ratio"]<1. compensate the thresholded matrix by a general principal component and the photocurrent profile.

--- run_stages/circuit_stage.py
+++ run_stages/circuit_stage.py
@@ -15,7 +15,7 @@ from circuit_building_blocks.electrode import SIROF
 from circuit_building_blocks.frame_driver import FramesDriver
 from circuit_building_blocks.image_driver import ImageDriver
 
-from utilities.image_processing_utilities import isEdge
+from utilities.image_processing_utilities import isEdge, Rmat_simp
 
 
 class CircuitStage(CommonRunStage):
@@ -30,6 +30,15 @@ class CircuitStage(CommonRunStage):
 		self.resistive_mesh = self.get_stage_output_func("resistive_mesh", 0)
 		self.number_of_pixels = Configuration().params["number_of_pixels"]
 		self.simulation_results = None
+
+		self.G_comp_flag = False
+		if (Configuration().params['r_matrix_simp_ratio'] < 1) and (Configuration().params["model"]== Models.MONOPOLAR.value):
+			imag_basis = np.array(self.video_sequence["Frames"]).reshape((self.number_of_pixels, -1))
+			col_norm = np.linalg.norm(imag_basis, axis=0)
+			imag_basis = imag_basis[:, col_norm>1E-6]
+			(self.resistive_mesh, self.G_comp) = Rmat_simp(Rmat=self.resistive_mesh,
+						  ratio=Configuration().params['r_matrix_simp_ratio'], imag_basis=imag_basis)
+			self.G_comp_flag = True
 
 		if Configuration().params.get("r_matrix_input_file_px_pos"):
 			px_pos = np.loadtxt(Configuration().params["r_matrix_input_file_px_pos"], delimiter=',')
@@ -105,28 +114,41 @@ class CircuitStage(CommonRunStage):
 			#configure the active electrodes and their connections to the GND
 			self.circuit.V(f'CProbe{px_idx}', f'Si{px_idx}', f'Pt{px_idx}', 0 @U.u_V)  # current probe
 			self.circuit.X(f'Active{px_idx}', f"active PARAMS: Vini={Configuration().params['Vini_act'][px_idx-1]}", f'Pt{px_idx}', f'Saline{px_idx}')
-			self.circuit.R(f'{px_idx}_{px_idx}', f'Saline{px_idx}', f'Saline{0}',self.resistive_mesh[px_idx - 1, px_idx - 1] @ U.u_Ohm)
+			self.circuit.R(f'{px_idx}_{px_idx}', f'Saline{px_idx}', f'Saline{0}', "{:.3e}".format(self.resistive_mesh[px_idx - 1, px_idx - 1]))
 			
 			if isBipolar:
 				#configure return electrodes and their connections to the GND
 				self.circuit.V(f'rCProbe{px_idx}', self.circuit.gnd, f'rPt{px_idx}', 0@U.u_V)
 				self.circuit.X(f'Return{px_idx}', 'return' + ("_edge" if self.is_edge[px_idx-1] else ""), f'rPt{px_idx}', f'rSaline{px_idx}')
 				
-				self.circuit.R(f'r{px_idx}_{px_idx}', f'rSaline{px_idx}', f'Saline{0}', self.resistive_mesh[px_idx+self.number_of_pixels-1, px_idx+self.number_of_pixels-1]@U.u_Ohm)
+				self.circuit.R(f'r{px_idx}_{px_idx}', f'rSaline{px_idx}', f'Saline{0}',
+		   						"{:.3e}".format(self.resistive_mesh[px_idx+self.number_of_pixels-1, px_idx+self.number_of_pixels-1]))
 				# connections between each pair of active and return
 				for cross_idx in range(1, self.number_of_pixels+1):
 					self.circuit.R(f'ar{cross_idx}_{px_idx}', f'Saline{cross_idx}', f'rSaline{px_idx}',
-						self.resistive_mesh[px_idx-1, cross_idx+self.number_of_pixels-1]@U.u_Ohm)
+						"{:.3e}".format(self.resistive_mesh[px_idx-1, cross_idx+self.number_of_pixels-1]))
 
 			# now define the interconnected resistor mesh
 			for cross_idx in range(1, px_idx):
+				R = self.resistive_mesh[px_idx - 1, cross_idx - 1]
+				if np.isnan(R):
+					continue
 				#interconnection among the active electrodes
-				self.circuit.R(f'{cross_idx}_{px_idx}', f'Saline{cross_idx}', f'Saline{px_idx}',
-							   "{:.3e}".format(self.resistive_mesh[px_idx - 1, cross_idx - 1]))
+				self.circuit.R(f'{cross_idx}_{px_idx}', f'Saline{cross_idx}', f'Saline{px_idx}', "{:.3e}".format(R))
 				if isBipolar:
 					#interconnection among the return electrodes
 					self.circuit.R(f'r{cross_idx}_{px_idx}', f'rSaline{cross_idx}', f'rSaline{px_idx}',  
 		    			"{:.3e}".format(self.resistive_mesh[px_idx+self.number_of_pixels-1, cross_idx+self.number_of_pixels-1]))
+			
+		# Conductance matrix compensation after thresholding:
+		if self.G_comp_flag:
+			for comp_idx in range(self.G_comp['v_basis'].shape[1]):
+				self.circuit.R(f'comp{comp_idx}', f'comp{comp_idx}', self.circuit.gnd, 1)
+				for px_idx in range(1, self.number_of_pixels + 1):
+					self.circuit.VCCS(f'px{px_idx}_comp{comp_idx}', self.circuit.gnd, f'comp{comp_idx}',
+		       							f'Saline{px_idx}', f'Saline{0}', "{:.3e}".format(self.G_comp['v_basis'][px_idx-1, comp_idx]))
+					self.circuit.VCCS(f'comp{comp_idx}_px{px_idx}', f'Saline{px_idx}', f'Saline{0}',
+		       							f'comp{comp_idx}', self.circuit.gnd, "{:.3e}".format(self.G_comp['i_basis'][px_idx-1, comp_idx]))
 		
 		return self.circuit
 
