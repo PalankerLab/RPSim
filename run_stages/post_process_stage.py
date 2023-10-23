6d5c06af2456a280a9da77646052228888df34c2
Anna <59121577+annako13@users.noreply.github.com>
2023-10-22 22:58:29 -0700

    add current pulse extraction from on diodes, enable to avoid field averaging in post process

--- run_stages/post_process_stage.py
+++ run_stages/post_process_stage.py
@@ -1,6 +1,7 @@
 import glob
 import multiprocessing
 import os
+from collections import OrderedDict
 
 import matplotlib
 from PIL import Image
@@ -38,7 +39,8 @@ class PostProcessStage(CommonRunStage):
 			# extract analysis parameters
 			number_of_pixels = Configuration().params["number_of_pixels"]
 			self.start_time_ms = Configuration().params["pulse_start_time_in_ms"]
-			self.pulse_duration = Configuration().params["stimulation_duration_in_ms"]
+			self.pulse_duration = Configuration().params["pulse_duration_in_ms"]
+			self.average_over_pulse_duration = Configuration().params["average_over_pulse_duration"]
 			self.averaging_resolution_ms = self.pulse_duration
 			self.time_points_to_analyze_ms = [self.start_time_ms]
 
@@ -49,7 +51,7 @@ class PostProcessStage(CommonRunStage):
 																step=self.averaging_resolution_ms))
 
 			# define depth resolution
-			default_depth_params =  [x*1 for x in range(150)] #[x * 5 for x in range(11)] + [x * 5 + 57 for x in range(20)]
+			default_depth_params =  [x*1 for x in range(160)]
 			self.z_values = Configuration().params["depth_values_in_um"] if Configuration().params.get("depth_values_in_um") else default_depth_params
 
 			# populate arrays with simulation values
@@ -91,13 +93,16 @@ class PostProcessStage(CommonRunStage):
 			# Va_time = np.zeros([len(times), N_pixels])
 			# Vr_time = np.zeros([len(times), N_pixels])
 
-	def _generate_potential_matrix_per_time_section(self, start_time):
+	def _generate_potential_matrix_per_time_section(self, start_time, average_over_pulse_duration=True):
 
 		# shift time vector to point of interest
 		shifted_time_vector = self.simulation_stage_output['time'] * 1E3 - start_time
 
+		# define end time based on whether we average over the pulse duration or not
+		end_time = self.averaging_resolution_ms if average_over_pulse_duration else 0.12
+
 		# filter the time indices that correspond to the current time window
-		time_indices_to_include = (shifted_time_vector > 0.1) & (shifted_time_vector < self.averaging_resolution_ms)
+		time_indices_to_include = (shifted_time_vector > 0.1) & (shifted_time_vector < end_time)
 		active_current_ua = self.full_active_current_ua[:, time_indices_to_include]
 		return_current_ua = self.full_return_current_ua[:, time_indices_to_include]
 		time_vector = shifted_time_vector[time_indices_to_include]
@@ -110,7 +115,6 @@ class PostProcessStage(CommonRunStage):
 
 		# initialize output structure for this time point
 		voltage_3d_matrix = np.zeros(self.xx.shape + (len(self.z_values),))
-
 
 		# calculate potential matrix for each z value in this time window
 		for z_index, z_value in enumerate(self.z_values):
@@ -178,6 +182,59 @@ class PostProcessStage(CommonRunStage):
 		plt.close()
 		return fig
 
+	@staticmethod
+	def _extract_diode_number(diode_name):
+		parts = diode_name.split("Pt")
+		number_part = int(parts[1])
+		return number_part
+
+	def _extract_on_diode_pulses(self):
+		# initialize output
+		on_diode_data_during_stable_pulse = OrderedDict()
+
+		# filter and sort the diodes
+		active_voltage_diodes = [key for key in self.simulation_stage_output.keys() if "Pt" in key and "rPt" not in key]
+		active_voltage_diodes = sorted(active_voltage_diodes, key=self._extract_diode_number)
+		active_voltage_diodes_numbers = [str(self._extract_diode_number(item)) for item in active_voltage_diodes]
+
+		# convert time array to ms
+		time_ms = self.simulation_stage_output['time'] * 1E3
+
+		# define a sttable pulse time window
+		time_window_start = Configuration().params["pulse_start_time_in_ms"] if Configuration().params["pulse_start_time_in_ms"] else 200
+		time_window_end = time_window_start + (Configuration().params["pulse_duration_in_ms"] if Configuration().params["pulse_duration_in_ms"] else 9.8)
+
+		# find the indices that correspond to the time window
+		indices_in_window = np.where((time_ms >= time_window_start) & (time_ms <= time_window_end))
+
+		# filter time vector to pulse window
+		time_ms = time_ms[indices_in_window]
+
+		# add time entry to output dictionary
+		on_diode_data_during_stable_pulse["time_ms"] = time_ms
+
+		# set the threshold for diode activation
+		threshold = Configuration().params["on_diode_threshold_mV"] if Configuration().params["on_diode_threshold_mV"] else 50
+
+		# search for diodes that are active within the needed pulse window
+		for diode_number in active_voltage_diodes_numbers:
+
+			# convert this diodes voltage to mV
+			voltage_mv = self.simulation_stage_output['Pt' + diode_number] * 1E3
+			current_ua = self.simulation_stage_output['VCProbe' + diode_number] * 1E6
+
+			# extract a section that corresponds to a stable pulse
+			voltage_in_time_window = voltage_mv[indices_in_window]
+			current_in_time_window = current_ua[indices_in_window]
+
+			# check if diode is on, and if so, add data to output
+			if max(voltage_in_time_window) > threshold:
+				on_diode_data_during_stable_pulse[diode_number] = OrderedDict()
+				on_diode_data_during_stable_pulse[diode_number]["current"] = current_in_time_window
+				on_diode_data_during_stable_pulse[diode_number]["voltage"] = voltage_in_time_window
+
+		return on_diode_data_during_stable_pulse
+
 	# def _generate_gif_data(self):
 	# 	{"gif_data": self.gif_image, "gif_time": self.gif_time}, self.image_sequence_input_folder]
 	# 	frames = [Image.open(image) for image in sorted(glob.glob(f"{self.output_directory}/*.png"),key=os.path.getmtime)]
@@ -194,10 +251,17 @@ class PostProcessStage(CommonRunStage):
 		"""
 		self.z_values = None
 
+		# find the diodes that are on
+		on_diode_data_during_stable_pulse = self._extract_on_diode_pulses()
+
 		if Configuration().params["model"] == Models.MONOPOLAR.value:
 
 			pulse_width = Configuration().params["pulse_duration_ms"]
 			pulse_start = Configuration().params["pulse_start_time_in_ms"]
+			average_over_pulse_duration = Configuration().params["average_over_pulse_duration"]
+
+			# define end time based on whether we average over the pulse duration or not
+			end_time = pulse_width if average_over_pulse_duration else 0.12
 
 			V_dict = np.loadtxt(Configuration().params["r_matrix_input_file_active"], delimiter=',')  # mV
 			X_act = V_dict[0, :]
@@ -237,7 +301,7 @@ class PostProcessStage(CommonRunStage):
 			I_act = np.sum(I_act * Td, axis=1) / np.sum(Td)
 			I_ret = np.sum(I_ret * Td) / np.sum(Td)
 
-			self.z_values = [x*1 for x in range(151)] #[x * 5 for x in range(11)] + [x * 5 + 57 for x in range(10)]
+			self.z_values = [x*1 for x in range(160)]
 			voltage_3d_matrix = np.zeros(XX.shape + (len(self.z_values),))
 
 			# create voltage x-y matrix for each z plane
@@ -260,7 +324,8 @@ class PostProcessStage(CommonRunStage):
 								 "3d_mesh_um": None,
 								 "z_um": self.z_values,
 								 "t_ms": [0],
-								 "pixel_coordinates_um": px_pos}
+								 "pixel_coordinates_um": px_pos,
+								 "on_diode_data": on_diode_data_during_stable_pulse}
 
 			#frame_width = x_frame.size
 			frame_width = abs(x_frame[0])
@@ -274,11 +339,12 @@ class PostProcessStage(CommonRunStage):
 			voltage_4d_matrix = np.zeros(self.xx.shape + (len(self.z_values),) + (len(self.time_points_to_analyze_ms),))
 
 			output_dictionary = {"v(x,y,z,t)_mv": None,
-									  "2d_mesh_um": (self.xx, self.yy),
-									  "3d_mesh_um": (self.xxx, self.yyy, self.zzz),
-									  "z_um": self.z_values,
-									  "t_ms": self.time_points_to_analyze_ms,
-									  "pixel_coordinates_um": self.pixel_coordinates}
+									"2d_mesh_um": (self.xx, self.yy),
+									"3d_mesh_um": (self.xxx, self.yyy, self.zzz),
+									"z_um": self.z_values,
+									"t_ms": self.time_points_to_analyze_ms,
+									"pixel_coordinates_um": self.pixel_coordinates,
+									"on_diode_data": on_diode_data_during_stable_pulse}
 
 			# # run post-processing concurrently for all the time points
 			# with Pool(multiprocessing.cpu_count()//3) as pool:
@@ -290,7 +356,8 @@ class PostProcessStage(CommonRunStage):
 			# iterate over time and create 3D potential matrix for each time point
 			for time_point_index, time_point_value in enumerate(self.time_points_to_analyze_ms):
 				# calculate metrix for this time point
-				voltage_3d_matrix = self._generate_potential_matrix_per_time_section(start_time=time_point_value)
+				voltage_3d_matrix = self._generate_potential_matrix_per_time_section(start_time=time_point_value,
+																					 average_over_pulse_duration=self.average_over_pulse_duration)
 				# add result to output structure
 				voltage_4d_matrix[:, :, :, time_point_index] = voltage_3d_matrix
 
